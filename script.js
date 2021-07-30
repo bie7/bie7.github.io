290fdce938ef6a130444a183e0f3cd44aec0de33
bie7 <habibi.bie77@gmail.com>
2021-07-30 15:04:44 +0700

    pembaruan kontak dan alert

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
   const scriptURL = 'https://script.google.com/macros/s/AKfycbyKS-gHzhkmTulHFq1HYfCCgz5emp7Ph0_sJSFgs-fnb0w6yDQQrky21c1m6NMRLaHN/exec'
   const form = document.forms['contact-form']
-  const hide = document.querySelectorAll('.hidden');
+  const load = document.querySelector('.load');
+  const btnAsli = document.querySelector('.btn-asli');
 
   form.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
+    // tampilkan loading saat blm sukses
+    load.classList.toggle('hidden')
+    btnAsli.classList.toggle('hidden')
+
     fetch(scriptURL, {
         method: 'POST',
         body: new FormData(form)
       })
       .then(response => {
+        // tampilkan kembali tombol kirirm
+        load.classList.toggle('hidden');
+        btnAsli.classList.toggle('hidden');
+
         // tampilkan callback pada saat pengiriman data form sukses
-        // M.toast({
-        //   html: 'Pesan Anda Telah Terkirim !'
-        // });
         Swal.fire({
           icon: 'success',
           title: 'Data Anda Sudah Terkirim',
           showConfirmButton: false,
-          timer: 1500
+          timer: 2000
         });
         // reset form setelah berhasil terkirim
         form.reset();
@@ -28,7 +34,7 @@
           icon: 'error',
           title: 'Terjadi Kesalahan',
           showConfirmButton: false,
-          timer: 1500
+          timer: 2000
         });
         console.error('Error!', error.message)
       })
