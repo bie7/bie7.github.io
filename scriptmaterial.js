6a68215eeec5d05f35a555b1b1808bd2cc10192e
bie7 <habibi.bie77@gmail.com>
2021-07-30 10:36:33 +0700

    pembaruan pada text area dan file js

---
+++ scriptmaterial.js
@@ -0,0 +1,31 @@
+ // navbar initialization
+ document.addEventListener('DOMContentLoaded', function () {
+     let elems = document.querySelectorAll('.sidenav');
+     M.Sidenav.init(elems);
+ });
+
+
+ // slider
+ let slider = document.querySelectorAll('.slider');
+ M.Slider.init(slider, {
+     height: 400,
+     interval: 4000
+ });
+ // parallax
+ let parallax = document.querySelectorAll('.parallax');
+ M.Parallax.init(parallax);
+
+ // materialbox
+ document.addEventListener('DOMContentLoaded', function () {
+     let materialbox = document.querySelectorAll('.materialboxed');
+     M.Materialbox.init(materialbox);
+ });
+ // scoll
+ let scroll = document.querySelectorAll('.scrollspy');
+ M.ScrollSpy.init(scroll, {
+     scrollOffset: 63
+ });
+
+ // textarea lenght
+ let textArea = document.getElementById('textarea2');
+ M.CharacterCounter.init(textArea);
